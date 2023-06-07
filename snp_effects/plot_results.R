3718fc8f476a4bd6332070e3badc6bb1337dec82
peter <petrel.harp@gmail.com>
2023-06-06 21:55:36 -0700

    more plotting

--- snp_effects/plot_results.R
+++ snp_effects/plot_results.R
@@ -10,6 +10,8 @@ manifest <- subset(read.csv("gwas_manifest.csv", header=TRUE, na.string="N/A", s
 doubles <- names(table(manifest$Phenotype.Code))[table(manifest$Phenotype.Code) == 2]
 manifest <- manifest[-(match(doubles,manifest$Phenotype.Code)),]
 
+phenotypes <- read.table("phenotypes.both_sexes.tsv", stringsAsFactors=FALSE, sep="\t", quote="", header=TRUE, comment.char="")
+
 ## First run with prop 25%
 results <- read.table("snp_results_25.tsv")
 pdf(file="results_25.pdf", width=6, height=8, pointsize=10)
@@ -24,31 +26,97 @@ dev.off()
 ## Next run with prop 10%
 results <- read.table("snp_results_10.tsv")
 results$name <- manifest$Phenotype.Description[match(results$code, manifest$Phenotype.Code)]
+results$name[results$name == "NA"] <- NA
 results <- subset(results, !grepl("Job code", results$name))
 results <- subset(results, !grepl("Type of", results$name)) # seems to be all diet-related
 
-pdf(file="results_10.pdf", width=6, height=8, pointsize=10)
-layout((1:2))
-with(subset(results, slope > -4), {
-    hist(slope, breaks=50, xlab='exponent', main="tail proportion: 10%")
-    plot(nsnps, slope, pch=20, cex=0.5, col=adjustcolor('black', 0.5))
-})
-dev.off()
+pheno_names <- c("phenotype", "n_non_missing", "n_missing", "n_controls", "n_cases")
+pheno <- phenotypes[,pheno_names]
+names(pheno) <- c("code", pheno_names[-1])
+stopifnot(all(results$code %in% pheno$code))
+results <- merge(results, pheno, all.x=TRUE)
+# very common things (with > 10000 cases) have Something Different going on
+results$pruned <- !(results$n_non_missing > 300000 & results$n_cases < 10000)
 
 categories <- c(
     "Non-cancer illness code",
-    "Cancer code",
     "neoplasm",
-    "ICD10"
+    "ICD10",
+    "other"
+)
+results$category <- ifelse(
+    grepl(categories[1], results$name), categories[1],
+    ifelse(
+        grepl(categories[2], results$name), categories[2],
+        ifelse(
+            grepl(categories[3], results$name), categories[3], categories[4]
+        )
+    )
 )
 
-pdf("cat_results_10.pdf", width=6, height=9)
 for (cname in categories) {
-    with(subset(results, grepl(cname, name)), {
-        layout((1:3))
-        hist(slope, breaks=50, xlab='exponent', main=cname)
-        hist(slope[slope > -4], breaks=50, xlab='exponent', main="tail proportion: 10%")
-        plot(nsnps, slope, pch=20, cex=0.5, col=adjustcolor('black', 0.5))
+    pdf(sprintf("%s_results_10.pdf", gsub(" ", "_", cname)), width=7, height=6)
+    with(subset(results, category == cname), {
+        layout(matrix(c(1,2,1,3,1,4), nrow=2))
+        hist(slope, breaks=30, xlab='exponent', main=cname)
+        plot(nsnps, slope, pch=20, cex=0.5, xlab='number of snps',
+             col=adjustcolor(ifelse(pruned, "red", "black"), 0.5))
+        plot(n_cases, slope, pch=20, cex=0.5, xlab='number of cases',
+             col=adjustcolor(ifelse(pruned, "red", "black"), 0.5))
+        plot(n_non_missing, slope, pch=20, cex=0.5, xlab='number of non-missing subjects',
+             col=adjustcolor(ifelse(pruned, "red", "black"), 0.5))
     })
+    dev.off()
+}
+
+
+pdf(file="results_10.pdf", width=6, height=8, pointsize=10)
+layout(matrix(c(1,2,1,3), nrow=2))
+with(subset(results, !pruned), {
+    hist(slope, breaks=50, xlab='exponent', main="tail proportion: 10%")
+    plot(nsnps, slope, pch=20, cex=0.5, col=adjustcolor('black', 0.5), log='x')
+    plot(n_cases, slope, pch=20, cex=0.5, col=adjustcolor('black', 0.5), log='x')
+})
+dev.off()
+
+pdf(file="slopes_by_category.pdf", width=6, height=9, pointsize=10)
+layout(seq_along(categories))
+breaks <- hist(results$slope[!results$pruned], breaks=30, plot=FALSE)$breaks
+for (cname in categories) {
+    with(subset(results, !pruned & category == cname), {
+        hist(slope, breaks=breaks, main=cname)
+    })
+}
+dev.off()
+
+plot_code <- function (code) {
+    ij <- match(code, results$code)
+    if (is.na(ij)) stop(sprintf("Code %s not in results.", code))
+    code_dir <- file.path("phenotypes", code)
+    xlm_file <- file.path(code_dir, paste0("tail_10_", "xlm.RData"))
+    if (!file.exists(xlm_file)) stop(sprintf("File %s does not exist.", xlm_file))
+    logx <- xlm$model[["log(x)"]]
+    logy <- xlm$model[["log(tail_prob)"]]
+    plot(logx, logy, ylab='log(prob > x)', xlab='log(x)', main=results$name[ij], sub=code)
+    abline(coef(xlm))
+    legend("topright", lty=c(1, NA, NA),
+           legend=c(sprintf("y = %0.2f %0.2f x", coef(xlm)[1], coef(xlm)[2]),
+                    sprintf("num snps = %d", results$nsnps[ij]),
+                    sprintf("num cases = %d", results$n_cases[ij])
+           )
+    )
+}
+
+set.seed(12)
+examples <- lapply(categories, function (cname) {
+       with(subset(results, category==cname & !pruned & !is.na(name)), {
+            xc <- cut(slope, c(-100,-2,-1.5,0))
+            tapply(code, xc, sample, 1)
+       })
+})
+
+pdf(file="examples.pdf", width=7, height=9, pointsize=10)
+for (code in unlist(examples)) {
+    plot_code(code)
 }
 dev.off()
