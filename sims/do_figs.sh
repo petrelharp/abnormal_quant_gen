7258c63c7ac14bd42884e265ee66952efc826eb6
Peter Ralph <petrel.harp@gmail.com>
2023-06-20 19:59:55 -0700

    .

--- sims/do_figs.sh
+++ sims/do_figs.sh
@@ -2,11 +2,11 @@
 
 for type in normal cauchy
 do
-    for xopt in 0.0 1.0 2.0 10.0
+    for xopt in 0 1 2 10
     do
         for seed in 123 456 789
         do
-            sbatch run_sim.srun "-s ${seed} -d BURNIN=40000 -d NUMGENS=10000 -d N=10000 -d XOPT=${xopt} ${type}.slim"
+            sbatch run_sim.srun "-s ${seed}${xopt} -d BURNIN=40000 -d NUMGENS=10000 -d N=10000 -d XOPT=${xopt}.0 ${type}.slim"
         done
     done
 done
