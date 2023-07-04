98c4d15472946f63279aeac5ef3f4016f0eb270a
Peter Ralph <petrel.harp@gmail.com>
2023-07-03 22:48:44 -0700

    .

--- sims/do_figs.sh
+++ sims/do_figs.sh
@@ -2,11 +2,12 @@
 
 for type in normal cauchy
 do
-    for xopt in 0 1 2 10
+    for seed in 8135 8791
     do
-        for seed in 1024 4057 7090
+        sbatch run_sim.srun "-s ${seed}66${xopt} -d BURNIN=40000 -d NUMGENS=10000 -d N=10000 neutral_${type}.slim"
+        for xopt in 0 1 2 10
         do
-            sbatch run_sim.srun "-s ${seed}88${xopt} -d BURNIN=40000 -d NUMGENS=1000 -d N=10000 -d STEP=10 -d XOPT=${xopt}.0 ${type}.slim"
+            sbatch run_sim.srun "-s ${seed}55${xopt} -d BURNIN=40000 -d NUMGENS=10000 -d N=10000 -d STEP=10 -d XOPT=${xopt}.0 ${type}.slim"
         done
     done
 done
--- sims/make_paper_figs.sh
+++ sims/make_paper_figs.sh
@@ -1,5 +1,9 @@
-slim -s 831 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 neutral_cauchy.slim
-slim -s 831 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 neutral_normal.slim
+#!/bin/bash
+SLIM="sbatch run_sim.srun"
+# SLIM=slim
 
-slim -s 707 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d STEP=20 cauchy.slim
-slim -s 707 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d STEP=20 normal.slim
+$SLIM -s 831 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d 'EPSILON=c(0,0.1,10)' neutral_cauchy.slim
+$SLIM -s 831 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d 'EPSILON=c(0,0.1,10)' neutral_normal.slim
+
+$SLIM -s 707 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d STEP=20 -d 'EPSILON=c(0,0.1,10)' cauchy.slim
+$SLIM -s 707 -d BURNIN=20000 -d NUMGENS=1000 -d N=1000 -d STEP=20 -d 'EPSILON=c(0,0.1,10)' normal.slim
