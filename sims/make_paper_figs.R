421869ffe0084f55af0f15a7b5066aea6cce4736
peter <petrel.harp@gmail.com>
2023-07-03 22:26:07 -0700

    .

--- sims/make_paper_figs.R
+++ sims/make_paper_figs.R
@@ -27,57 +27,6 @@ load_files <- function (base) {
     ))
 }
 
-# 1. Plots of median trait value over time and final trait distribution
-#  for neutral Normal and Cauchy examples
-fname <- "neutral_trait_traces.pdf"
-examples <- lapply(list(
-                 "neutral Normal" = "sim_neutral_normal_831",
-                 "neutral Cauchy" = "sim_neutral_cauchy_831"
-    ), load_files)
-labels <- list(c("(A)", "(B)"), c("(C)", "(D)"))
-names(labels) <- names(examples)
-
-pdf(file=fname, width=6.5, height=3, pointsize=10)
-layout(matrix(1:4, nrow=2), heights=c(1.5,1))
-for (x in names(examples)) {
-    par(mar=c(4,4,2,0)+.1, mgp=c(2.5,1,0))
-        plot(med_trait ~ time, data=examples[[x]][['fix']], type='l',
-             main=x, xlab='time', ylab='median trait value')
-    mtext(labels[[x]][1], 3, adj=-0.0, line=0.2)
-    par(mar=c(4,4,0,0)+.1, mgp=c(2.5,1,0))
-        hist(examples[[x]][['pop']]$trait, breaks=100,
-             xlab='trait value', main='')
-    mtext(labels[[x]][2], 3, adj=-0.0, line=0.2)
-}
-dev.off()
-
-# 2. Plots of median trait value over time and final trait distribution
-#  for selected Normal and Cauchy examples
-fname <- "selected_trait_traces.pdf"
-examples <- lapply(list(
-                 "selected Normal" = "sim_normal_707",
-                 "selected Cauchy" = "sim_cauchy_707"
-    ), load_files)
-labels <- list(c("(A)", "(B)"), c("(C)", "(D)"))
-names(labels) <- names(examples)
-
-pdf(file=fname, width=6.5, height=3, pointsize=10)
-layout(matrix(1:4, nrow=2), heights=c(1.5,1))
-for (x in names(examples)) {
-    par(mar=c(4,4,2,0)+.1, mgp=c(2.5,1,0))
-        plot(med_trait ~ time, data=examples[[x]][['fix']], type='l',
-             main=x, xlab='time', ylab='median trait value')
-    mtext(labels[[x]][1], 3, adj=-0.0, line=0.2)
-    par(mar=c(4,4,0,0)+.1, mgp=c(2.5,1,0))
-        hist(examples[[x]][['pop']]$trait, breaks=100,
-             xlab='trait value', main='')
-    mtext(labels[[x]][2], 3, adj=-0.0, line=0.2)
-}
-dev.off()
-
-# 3. Segregation noise:
-# (distrn of seg noise), (seg noise vs midparent), and (Radon-Nicodym deriv of conditional seg noise for a few values of midparent)
-
 plot_seg_noise <- function (fname, examples, labels, seg_col, main_append="") {
     png(file=fname, width=6.5, height=6, pointsize=10, units='in', res=288)
     layout(matrix(1:6, nrow=3), heights=c(1.5,1, 1))
@@ -105,25 +54,85 @@ plot_seg_noise <- function (fname, examples, labels, seg_col, main_append="") {
     dev.off()
 }
 
-# Neutral
 
-fname <- "neutral_seg_noise.png"
+###########
+# Neutral examples
 examples <- lapply(list(
                  "neutral Normal" = "sim_neutral_normal_831",
                  "neutral Cauchy" = "sim_neutral_cauchy_831"
     ), load_files)
+
+# 1. Plots of median trait value over time and final trait distribution
+fname <- "neutral_trait_traces.pdf"
+labels <- list(c("(A)", "(B)"), c("(C)", "(D)"))
+names(labels) <- names(examples)
+
+pdf(file=fname, width=6.5, height=3, pointsize=10)
+layout(matrix(1:4, nrow=2), heights=c(1.5,1))
+for (x in names(examples)) {
+    par(mar=c(4,4,2,0)+.1, mgp=c(2.5,1,0))
+        plot(med_trait ~ time, data=examples[[x]][['fix']], type='l',
+             main=x, xlab='time', ylab='median trait value')
+    mtext(labels[[x]][1], 3, adj=-0.0, line=0.2)
+    par(mar=c(4,4,0,0)+.1, mgp=c(2.5,1,0))
+        hist(examples[[x]][['pop']]$trait, breaks=100,
+             xlab='trait value', main='')
+    mtext(labels[[x]][2], 3, adj=-0.0, line=0.2)
+}
+dev.off()
+
+# 3. Segregation noise:
+# (distrn of seg noise), (seg noise vs midparent), and (Radon-Nicodym deriv of conditional seg noise for a few values of midparent)
+
+fname <- "neutral_seg_noise.png"
 labels <- list(c("(A)", "(B)", "(C)"), c("(D)", "(E)", "(F)"))
 names(labels) <- names(examples)
 
 plot_seg_noise(fname, examples, labels)
 
-# Selected
+# 4. Segregation noise for small effects
+# (distrn of seg noise), (seg noise vs midparent), and (Radon-Nicodym deriv of conditional seg noise for a few values of midparent)
+# ONLY for small effects
 
-fname <- "selected_seg_noise.png"
+fname <- "neutral_seg_noise_small.png"
+labels <- list(c("(A)", "(B)", "(C)"), c("(D)", "(E)", "(F)"))
+names(labels) <- names(examples)
+
+plot_seg_noise(fname, examples, labels, seg_col=1)
+
+
+##########
+#  Examples with selection
 examples <- lapply(list(
                  "selected Normal" = "sim_normal_707",
                  "selected Cauchy" = "sim_cauchy_707"
     ), load_files)
+
+# 1. Plots of median trait value over time and final trait distribution
+fname <- "selected_trait_traces.pdf"
+labels <- list(c("(A)", "(B)"), c("(C)", "(D)"))
+names(labels) <- names(examples)
+
+pdf(file=fname, width=6.5, height=3, pointsize=10)
+layout(matrix(1:4, nrow=2), heights=c(1.5,1))
+for (x in names(examples)) {
+    par(mar=c(4,4,2,0)+.1, mgp=c(2.5,1,0))
+        plot(med_trait ~ time, data=examples[[x]][['fix']], type='l',
+             main=x, xlab='time', ylab='median trait value')
+    mtext(labels[[x]][1], 3, adj=-0.0, line=0.2)
+    par(mar=c(4,4,0,0)+.1, mgp=c(2.5,1,0))
+        hist(examples[[x]][['pop']]$trait, breaks=100,
+             xlab='trait value', main='')
+    mtext(labels[[x]][2], 3, adj=-0.0, line=0.2)
+}
+dev.off()
+
+# Selected
+
+# 3. Segregation noise:
+# (distrn of seg noise), (seg noise vs midparent), and (Radon-Nicodym deriv of conditional seg noise for a few values of midparent)
+
+fname <- "selected_seg_noise.png"
 labels <- list(c("(A)", "(B)", "(C)"), c("(D)", "(E)", "(F)"))
 names(labels) <- names(examples)
 
@@ -134,26 +143,7 @@ plot_seg_noise(fname, examples, labels)
 # (distrn of seg noise), (seg noise vs midparent), and (Radon-Nicodym deriv of conditional seg noise for a few values of midparent)
 # ONLY for small effects
 
-
-# Neutral
-
-fname <- "neutral_seg_noise_small.png"
-examples <- lapply(list(
-                 "neutral Normal" = "sim_neutral_normal_831",
-                 "neutral Cauchy" = "sim_neutral_cauchy_831"
-    ), load_files)
-labels <- list(c("(A)", "(B)", "(C)"), c("(D)", "(E)", "(F)"))
-names(labels) <- names(examples)
-
-plot_seg_noise(fname, examples, labels, seg_col=1)
-
-# Selected
-
 fname <- "selected_seg_noise_small.png"
-examples <- lapply(list(
-                 "selected Normal" = "sim_normal_707",
-                 "selected Cauchy" = "sim_cauchy_707"
-    ), load_files)
 labels <- list(c("(A)", "(B)", "(C)"), c("(D)", "(E)", "(F)"))
 names(labels) <- names(examples)
 
